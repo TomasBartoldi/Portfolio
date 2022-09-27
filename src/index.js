c6240366da579d3bc7e54bef3bbbb91b77a2e111
Tomás <bartoldit@gmail.com>
2022-09-27 12:52:20 -0300

    Init

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+  <BrowserRouter>
+     <App />
+  </BrowserRouter>,
+  document.getElementById('root')
 );
 
 
