523e554c68f5a6f83990518c20393f2f3a814abf
Tomás <bartoldit@gmail.com>
2022-09-27 13:19:13 -0300

    Starting Routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
 import './index.css'
+import Home from './routes/Home'
+import About from './routes/About'
+import Project from './routes/Project'
+import Contact from './routes/Contact'
+
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <h1>This is App.js</h1>
-    </div>
+    <>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/project' element={<Project />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+    </>
   );
 }
 
