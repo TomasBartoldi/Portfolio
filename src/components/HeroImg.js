ce98ec46dd7553cdd4bacb8f3679d02f54cf2de0
Tomás <bartoldit@gmail.com>
2022-09-27 16:22:07 -0300

    Heroe section finished

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -11,8 +11,8 @@ const HeroImg = () => {
            <img className='intro-img' src={IntroImg} alt='IntroImg'/>
         </div>
         <div className='content'>
-           <p>HI, I'M A FREELANCER.</p>
-           <h1>React Developer.</h1>
+           <p>HI, I'M TOMÁS BARTOLDI.</p>
+           <h1>Front-End Developer.</h1>
            <div>
              <Link to='/project' className='btn'>Projects</Link>
              <Link to='/contact' className='btn btn-light'>Contact</Link>
