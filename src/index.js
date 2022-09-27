b1863c75cfed6e81e465bc835cb55085018a5faa
Tomás <bartoldit@gmail.com>
2022-09-27 12:58:00 -0300

    Folders

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <BrowserRouter>
-     <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+    <App />
+  </BrowserRouter>
 );
 
 
