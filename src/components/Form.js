1a3926776bfcd7c0fc45a15ac34e537c8121ab86
Tomás <bartoldit@gmail.com>
2022-12-06 00:55:56 -0300

    Form mailing

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,20 +5,22 @@ import React from 'react'
 const Form = () => {
   return (
     <div className='form'>
-        <form>
-           <label>Your Name</label>
-           <input type='text'></input>
+        <form   
+          action="https://formspree.io/f/mgeqgkvb"
+          method="POST">
+           <label for="name">Your Name</label>
+           <input name="name" type='text'></input>
 
-           <label>Email</label>
-           <input type='email'></input>
+           <label for="name">Email</label>
+           <input name="email" type='email'></input>
 
-           <label>Subject</label>
-           <input type='text'></input>
+           <label for="name">Subject</label>
+           <input name="text" type='text'></input>
 
-           <label>Message</label>
-           <textarea rows='6' placeholder='Type your message here' />
+           <label for="name" >Message</label>
+           <textarea name="name" rows='6' placeholder='Type your message here' />
 
-           <button className='btn'>Submit</button>
+           <button type='submit' className='btn' target='blank'>Submit</button>
         </form>   
     </div>
   )
